324aca4dbd5e78d94a21ad3778ee6fee020d9398
tk <62570383+tksoftw@users.noreply.github.com>
2025-03-29 14:54:57 -0400

    upd

---
+++ test.py
@@ -0,0 +1,33 @@
+import requests
+
+# Base URL
+url = "https://hestia.jmrl.org/findit/Cart/doExport"
+
+# Headers
+headers = {
+    "accept": "*/*",
+    "accept-language": "en-US,en;q=0.9",
+    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "sec-ch-ua": '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+    "sec-ch-ua-mobile": "?1",
+    "sec-ch-ua-platform": '"Android"',
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "x-requested-with": "XMLHttpRequest"
+}
+start = 1_000_000
+# Print the response
+while start < 1_500_000:
+    # Data
+    data = {
+        'f': 'RIS',
+        'i[]': [f'Solr|{start+i}' for i in range(354)]
+    }
+
+    # Send POST request
+    response = requests.get(url, params=data, headers=headers)
+    with open("results.txt", "a", encoding='utf-8') as file:
+        file.write(response.text)
+    print(start)
+    start += 354

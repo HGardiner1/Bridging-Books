ed8580a33a202bdae2def27c47df2cdb6b1909d2
Nathan Mackintosh <natemack99@icloud.com>
2025-03-29 15:32:47 -0400

    reader

---
+++ marc_request.py
@@ -0,0 +1,34 @@
+import requests
+from file_parser import parse_file
+
+# Base URL
+url = "https://hestia.jmrl.org/findit/Cart/doExport"
+
+# Headers
+headers = {
+    "accept": "*/*",
+    "accept-language": "en-US,en;q=0.9",
+    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "sec-ch-ua": '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+    "sec-ch-ua-mobile": "?1",
+    "sec-ch-ua-platform": '"Android"',
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "x-requested-with": "XMLHttpRequest"
+}
+
+ids = parse_file("results2.txt")
+# Print the response
+for i in range(0,len(ids), 354):
+    # Data
+    data = {
+        'f': 'MARXML',
+        'i[]': [f'Solr|{ids[j]}' for j in range(1)]
+    }
+
+    # Send POST request
+    response = requests.get(url, params=data, headers=headers)
+    with open("marcxml-results2.txt", "a", encoding='utf-8') as file:
+        file.write(response.text)
+

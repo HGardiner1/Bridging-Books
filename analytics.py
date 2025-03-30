7fabf990d87c98ddadf9635d96260559ef0551d7
Nathan Mackintosh <natemack99@icloud.com>
2025-03-29 23:26:26 -0400

    fixed search funcs

--- analytics.py
+++ analytics.py
@@ -1,23 +1,33 @@
 
 import json
+from file_parser import get_cover_from_book
 
 def get_books_by_tag(tag: str, book_list) :
     result = []
     for book in book_list:
-        if book["tags"] and tag.lower() in map(lambda s : s.lower(), book["tags"]):
-            result.append(book)
+        if not book["tags"]:
+            continue
+        book["tags"] = [tag.lower() for tag in book["tags"]]
+        for book_tag in book["tags"]:
+            if tag.lower() in book_tag:
+                result.append(book)
     return result
 
 def get_books_by_genre(genre: str, book_list) :
     result = []
     for book in book_list:
-        if book["genre"] and genre.lower() in map(lambda s : s.lower(), book["genre"]):
-            result.append(book)
+        if not book["genre"]:
+            continue
+        book["genre"] = [genre.lower() for genre in book["genre"]]
+        for book_genre in book["genre"]:
+            if genre.lower() in book_genre:
+                result.append(book)
     return result
 
 with open("main_json.json") as file:
     dict = json.load(file)
     books = dict["books"]
 
-books = get_books_by_genre("fiction", books)
-print(books)
+result = get_books_by_tag("FROG", books)
+
+print(result)
--- file_parser.py
+++ file_parser.py
@@ -80,6 +80,9 @@ def get_cover(author: str, title: str, isbn: str, lib_id: int):
     title_encoded = parse.quote_plus(title)
     return f"https://hestia.jmrl.org/findit/Cover/Show?&size=large&recordid={lib_id}&source=Solr&isbn={isbn}&author={author_encoded}&title={title_encoded}"
 
+def get_cover_from_book(book):
+    get_cover(book["author"], book["title"], book["isbn"], book["lib_id"])
+
 def build_json(xml_txt_filename):
     final_json_dict = {'books':[]}
     for xml_string in get_xmls(xml_txt_filename):

b5c3c20aac461c0b8a09f4b91edad8db87e44ff4
Nathan Mackintosh <natemack99@icloud.com>
2025-03-29 23:03:06 -0400

    added get_books_by_genre()

--- analytics.py
+++ analytics.py
@@ -1,16 +1,23 @@
 
 import json
 
-def get_ids_by_tag(tag: str, book_list) :
-    ids = []
-    for book in books:
+def get_books_by_tag(tag: str, book_list) :
+    result = []
+    for book in book_list:
         if book["tags"] and tag.lower() in map(lambda s : s.lower(), book["tags"]):
-            ids.append(book["lib_id"])
-    return ids
+            result.append(book)
+    return result
+
+def get_books_by_genre(genre: str, book_list) :
+    result = []
+    for book in book_list:
+        if book["genre"] and genre.lower() in map(lambda s : s.lower(), book["genre"]):
+            result.append(book)
+    return result
 
 with open("main_json.json") as file:
     dict = json.load(file)
     books = dict["books"]
 
-ids = get_ids_by_tag("divorced women", books)
-print(ids)
+books = get_books_by_genre("fiction", books)
+print(books)
